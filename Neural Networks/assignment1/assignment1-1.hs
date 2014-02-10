ca3c900c5330632c01cd44baefb1b968965bfcb7
Alex Jeffery <apj21@sussex.ac.uk>
2014-02-10 17:09:19 +0000

    Began report, finished q1 code

--- Neural Networks/assignment1/assignment1-1.hs
+++ Neural Networks/assignment1/assignment1-1.hs
@@ -48,19 +48,30 @@ possibleLabellings :: [[LabelledInstance]]
 possibleLabellings = map (zip trainingData) labelCombos
 
 -- To learn a perceptron , we need initial weights, some training instances
--- (the [LabelledInstance]s), and a learning rate. The output is a function from
--- an instances to classes. We also take an integer parameter that is the number
--- of iterations we've done, and return this in a tuple with the learned
--- function.
-{--
-learnPerceptron :: Int -> Weights -> [LabelledInstance] -> Float -> (Int, (Instance -> Class))
-learnPerceptron iterNo weights trainingInstances learningRate =
-    if weights == newWeights then weights
-    else learnPerceptron (iterNo + 1) newWeights trainingInstances learningRate
+-- (the [ClassifiedInstance]s), a learning rate, and the error for the previous
+-- weights. The output is the learned weights, and the number of iterations. We
+-- also take an integer parameter that is the number of iterations we've done,
+-- and return this in a tuple with the learned weights.
+-- BATCH VERSION
+learnPerceptron :: Int -> Weights -> [ClassifiedInstance] ->
+    Float -> Float -> (Int, Weights)
+learnPerceptron iterNo (w1, w2) classifiedInstances learnRate prevError =
+    if changeInError < 0.001 && changeInError > -0.001 then
+        (iterNo, (w1, w2))
+    else
+        learnPerceptron (iterNo + 1) newWeights newClassifiedInstances
+            learnRate currentError
     where
+    newClassifiedInstances :: [ClassifiedInstance]
+    newClassifiedInstances =
+        map (classifyInstance newWeights) (map fst classifiedInstances)
     newWeights :: Weights
-    newWeights = someFunction
---}
+    newWeights = (
+        w1 - (learnRate * changeInError) ,
+        w2 - (learnRate * changeInError) )
+    currentError, changeInError :: Float
+    currentError = errorPerc (w1, w2) classifiedInstances
+    changeInError = currentError - prevError
 
 -- Implementation of the Perceptron Criterion error function
 errorPerc :: Weights -> [ClassifiedInstance] -> Float
@@ -79,6 +90,16 @@ errorPerc weights classifiedInstances = (-1) *
     where
     wronglyClassifiedInstances :: [ClassifiedInstance]
     wronglyClassifiedInstances = filter wronglyClassified classifiedInstances
+
+
+-- Make a ClassifiedInstance from a LabelledInstance by determining its Class
+-- from the given weights, and returning the LabelledInstance & Class together
+classifyInstance :: Weights -> LabelledInstance -> ClassifiedInstance
+classifyInstance w i = (i, (getClassOfInstance w (fst i)))
+
+-- Determine the class of an Instance based on the given weights
+getClassOfInstance :: Weights -> Instance -> Class
+getClassOfInstance w i = if (applyWeights w i) >= 0 then Pos else Neg
 
 -- Apply a weight vector to an instance - essentially the dot product for two
 -- vectors.
