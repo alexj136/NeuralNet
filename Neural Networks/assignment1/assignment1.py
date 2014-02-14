e775ede663a2b43ae69ac272828f8bb6dd33124e
Alex Jeffery <apj21@sussex.ac.uk>
2014-02-14 18:16:24 +0000

    WORKING PERCEPTRON

--- Neural Networks/assignment1/assignment1.py
+++ Neural Networks/assignment1/assignment1.py
@@ -30,6 +30,13 @@ class Weights:
     def length(self):
         return len(self.weights)
 
+    def __str__(self):
+        '''
+        Get a nice string representation of this Weights object
+        '''
+        return ''.join(['BIAS: ', str(self.bias), ', WEIGHTS: ',
+            str(self.weights)])
+
 def learn_perceptron(training_instances,
         wts = Weights(0.0, [0.0, 0.0]), learning_rate = 0.1):
     '''
@@ -42,31 +49,34 @@ def learn_perceptron(training_instances,
     while not converged:
         
         converged = True
-        erroneous_insts = [inst for inst in
-                training_instances if classify(wts, inst) is not inst.label]
 
-        if len(erroneous_insts) <= 0:
-            converged = False
-            errors = error(wts, erroneous_insts)
-            wts.bias = wts.bias - (learning_rate * errors.bias)
-            for i in range(len(wts.weights)):
-                wts.weights[i] = wts.weights[i] - (learning_rate * errors.weights[i])
+        for inst in training_instances:
+            if classify(wts, inst) is not inst.label:
+                converged = False
+                for i in range(len(wts.weights)):
+                    wts.weights[i] = wts.weights[i] + (
+                            learning_rate * inst.data[i] * inst.label)
+                wts.bias = wts.bias + (learning_rate * inst.label)
 
     return wts
-
+"""
 def error(wts, insts):
     '''
     The Perceptron Criterion error function
     '''
-    bias_err = error_single_dimension(wts.bias, [1 for i in insts], labels)
-    wts_errs  = []
+    wts_errs = [] # List to collect our error values for each dimension
+
     for i in range(wts.length()):
         dimension_values = []
         labels           = []
+
         for j in range(len(insts)):
             dimension_values.append(insts[i].data[j])
             labels.append(insts[i].label)
-        errors.append(error_single_dimension(wts.weights[i], dimension_weight, labels))
+
+        wts_errs.append(error_single_dimension(wts.weights[i], dimension_values, labels))
+
+    bias_err = error_single_dimension(wts.bias, [1 for i in insts], labels)
     return Weights(bias_err, wts_errs)
 
 def error_single_dimension(dimension_weight, dimension_values, labels):
@@ -76,11 +86,9 @@ def error_single_dimension(dimension_weight, dimension_values, labels):
     if len(dimension_values) is not len(labels):
         raise Exception('Mismatched number of instances and labels in error()')
 
-    total = 0
-    for i in range(len(dimension_values)):
-        total = total + (labels[i] * dimension_values[i] * dimension_weight)
-    return total
-
+    return sum([labels [i] * dimension_values[i] * dimension_weight
+        for i in range(len(dimension_values))])
+"""
 
 def classify(wts, inst):
     '''
@@ -100,9 +108,31 @@ def dot(x, y):
 
 if __name__ == "__main__":
     insts = [ Instance([0, 0], POSITIVE)
-        , Instance([1, 0], NEGATIVE)
-        , Instance([0, 1], POSITIVE)
-        , Instance([1, 1], NEGATIVE)
-        ]
+            , Instance([1, 0], NEGATIVE)
+            , Instance([0, 1], POSITIVE)
+            , Instance([1, 1], NEGATIVE)
+            ]
     wts = learn_perceptron(insts)
-    print "BIAS: ", wts.bias, ", WEIGHTS: ", wts.weights
+    for i in insts:
+        print 'INST: ', i.data, ' LABEL: ', i.label, ', CLASS: ', str(classify(wts, i))
+    print wts, '\n'
+
+    insts = [ Instance([0, 0], POSITIVE)
+            , Instance([1, 0], POSITIVE)
+            , Instance([0, 1], NEGATIVE)
+            , Instance([1, 1], NEGATIVE)
+            ]
+    wts = learn_perceptron(insts)
+    for i in insts:
+        print 'INST: ', i.data, ' LABEL: ', i.label, ', CLASS: ', str(classify(wts, i))
+    print wts, '\n'
+
+    insts = [ Instance([0, 0], POSITIVE)
+            , Instance([1, 0], NEGATIVE)
+            , Instance([0, 1], NEGATIVE)
+            , Instance([1, 1], POSITIVE)
+            ]
+    wts = learn_perceptron(insts)
+    for i in insts:
+        print 'INST: ', i.data, ' LABEL: ', i.label, ', CLASS: ', str(classify(wts, i))
+    print wts
