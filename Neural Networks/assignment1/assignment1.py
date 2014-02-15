7c1df8bc11a3feb95749e6fd3db1b5924582f7dd
Alex Jeffery <apj21@sussex.ac.uk>
2014-02-15 17:59:42 +0000

    cont

--- Neural Networks/assignment1/assignment1.py
+++ Neural Networks/assignment1/assignment1.py
@@ -56,6 +56,30 @@ def learn_perceptron(
 
     return (wts, iterations)
 
+def learn_regressor(
+          training_instances
+        , wts = Weights(0.0, [0.0, 0.0])
+        , learning_rate = 0.1
+        , iteration_cap = 100
+        ):
+    iterations = 0
+
+    error     = None
+    min_error = None
+    while iterations < iteration_cap and error <= min_error:
+        iterations = iterations + 1
+
+        # Calculate the current error, and if we've improved, set min_error
+        # equal to the current error. We stop iterating when we don't improve.
+        error = sum([dot(wts.weights, i.data) + wts.bias for i in training_instances])
+        if error < min_error: min_error = error
+
+        for inst in training_instances:
+            for i in range(len(wts.weights)):
+                wts.weights[i] = wts.weights[i] - (learning_rate * (wts.weights[i] - inst.data[i]))
+
+    return (wts, iterations)
+
 def classify(wts, inst):
     '''
     Get the class of an instance from the supplied weights - essentially the dot
@@ -107,10 +131,10 @@ def doPartA1():
     for insts in instance_sets:
         wts, iters = learn_perceptron(insts)
         for i in insts:
-            print 'INST: ', i.data, ' LABEL: ', i.label, ', CLASS: ', str(classify(wts, i))
+            print 'INST:', i.data, 'LABEL:', i.label, 'CLASS:', str(classify(wts, i))
         print wts, '\n', 'ITERATIONS: ', iters, '\n'
 
-def doPart1B():
+def doPartA2():
     # Generate random data as specified:
     posX1vals = [random.gauss(0, 1) for x in range(20)]
     posX2vals = [random.gauss(0, 1) for x in range(20)]
@@ -122,16 +146,40 @@ def doPart1B():
         insts.append(Instance([negX1vals[i], negX2vals[i]], NEGATIVE))
 
     # Train the perceptron
-    wts, instances = learn_perceptron(insts)
+    wts, iterations = learn_perceptron(insts)
 
-    # Generate two points on the decision boundary that we can use to draw the line
+    # Generate two points on the decision boundary that we can use to draw the
+    # line
     linex2s = [3.0, -3.0]
-    linex1s = [-1 * (wts.bias + (x2 * wts.weights[1]))/wts.weights[0] for x2 in linex2s]
+    linex1s = [-1 * (wts.bias + (x2 * wts.weights[1]))/wts.weights[0]
+            for x2 in linex2s]
+
+    print 'Decision boundary learnt in', iterations, 'iterations'
 
     # Plot the points & line
-    plot.plot(posX1vals, posX2vals, 'r^', negX1vals, negX2vals, 'b^', linex1s, linex2s, linewidth=1.0)
+    plot.plot(posX1vals, posX2vals, 'r^', negX1vals, negX2vals, 'g^',
+            linex1s, linex2s, linewidth=1.0)
     plot.show()
 
+def doPartB1():
+    # y = 0.4*x + 3 + delta, delta = uniform random from -10 to +10
+    data = [[x, (0.4 * x) + 3 + random.uniform(-10.0, 10.0)]
+            for x in range(1, 200, 2)]
+    insts = [Instance(d, 0) for d in data]
+    wts, iterations = learn_regressor(insts)
+    print wts, 'ITERS:', iterations
+    linex2s = [0.0, 90.0]
+    linex1s = [((wts.weights[0] * x2) - wts.bias)/wts.weights[1]
+            for x2 in linex2s]
+    plot.plot([d[0] for d in data], [d[1] for d in data], 'b^',
+            linex1s, linex2s, linewidth=1.0)
+    plot.show()
+
+def doPartB2():
+    pass
+
 if __name__ == "__main__":
-    doPartA1()
-    doPart1B()
+    #doPartA1()
+    #doPartA2()
+    doPartB1()
+    #doPartB2()
